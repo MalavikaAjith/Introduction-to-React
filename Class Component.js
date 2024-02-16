2b3b9037162dd99808d4475a90795925855a0b96
MalavikaAjith <126668248+MalavikaAjith@users.noreply.github.com>
2024-02-16 10:12:52 +0530

    Class Component.js

--- Class Component.js
+++ Class Component.js
@@ -9,3 +9,73 @@ class Carss extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Carss model="Mustang"/>);
+
+class Car extends React.Component {
+  render() {
+    return <h2>I am a Car!</h2>;
+  }
+}
+
+class Garage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Who lives in my Garage?</h1>
+        <Car/>
+      </div>
+    );
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Garage/>);
+
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>It is a {this.state.color} {this.state.model} from {this.state.year}.</p>
+      </div>
+    );
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Car/>);
+
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  changeColor = () => {
+    this.setState({color: "blue"});
+  }
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>It is a {this.state.color} {this.state.model} from {this.state.year}.</p>
+        <button type="button" onClick={this.changeColor}>Change Color</button>
+      </div>
+    );
+  }
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Car/>);
+
