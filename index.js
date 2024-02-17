fc08ae77c6469742cf416f645dfbc954effefc97
MalavikaAjith <126668248+MalavikaAjith@users.noreply.github.com>
2024-02-17 10:44:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,24 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
- class Car extends React.Component {
-  constructor() {
-    super();
-    this.state = {color : "red"};
-  }
-  render() {
-    return (
-      <div>
-        <h2>I am a {this.state.color} Car!</h2>
-      </div>
-    )
-  }
-}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Car/>
-  </React.StrictMode>
-);
+root.render(<App />);
+
 
