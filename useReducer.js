9dfada5696059213c72270361aff24e48f7c6444
MalavikaAjith <126668248+MalavikaAjith@users.noreply.github.com>
2024-02-19 11:41:55 +0530

    useReducer.js

---
+++ useReducer.js
@@ -0,0 +1,29 @@
+import React, {useReducer} from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function Reducer(state, action) {
+  if (action.type === 'incremented_age') {
+    return {
+      age: state.age + 1
+    };
+  }
+  throw Error('Unknown action.');
+}
+
+export default function Counter() {
+  const [state, dispatch] = useReducer(Reducer, { age: 42 });
+
+  return (
+    <>
+      <button onClick={() => {
+        dispatch({ type: 'incremented_age' })
+      }}>
+        Increment age
+      </button>
+      <p>Hello! You are {state.age}.</p>
+    </>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Counter />);
